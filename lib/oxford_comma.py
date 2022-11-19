bcb2834bc3d19f548d583b66d3fcae4dba22f26b
Dominique DeLeon <dominique.a.deleon@gmail.com>
2022-11-18 21:34:16 -0500

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    sz = len(items)
+    if sz == 1:
+        return items[0]
+    elif sz == 2:
+        return " and ".join(items)
+    else:
+        items[-1] = "and " + items[-1]
+        return ", ".join(items)
